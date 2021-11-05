a9640d31725765e2f99e8517d172cc2b64372e0e
Refractedd <bcwillia12@gmail.com>
2021-11-05 10:20:47 -0500

    Initial Project
    Stock Entity created

---
+++ src/Stock.java
@@ -0,0 +1,13 @@
+public class Stock {
+    private double price;
+    private int amount;
+
+    public Stock(){
+        this.price = 0.00;
+        this.amount = 0;
+    }
+    public Stock(double p, int a){
+        this.price = p;
+        this.amount = a;
+    }
+}
